7080da22cbfdd7346d2ba19726e34b2e1d3fc0f7
cloudjohnsp <jvavelino2017@gmail.com>
2024-10-22 20:52:28 -0300

    feat: creates browser router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from './Pages/Login/Login';
+
+const router = createBrowserRouter([
+  {
+    path: '/v1',
+    children: [
+      {
+        path: 'login',
+        element: <Login />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
