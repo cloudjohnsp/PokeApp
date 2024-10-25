6a6f1840236c03afb7c06bdbe645ea4910017f46
cloudjohnsp <jvavelino2017@gmail.com>
2024-10-25 20:07:11 -0300

    feat: creates custom hook to fetch pokemon data

---
+++ src/Hooks/usePokemon.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+import { TPokemonData } from '../types/types';
+
+const usePokemon = () => {
+  const [pokemonData, setPokemonData] = useState<TPokemonData[]>([]);
+
+  const pokemonUrl = (id: number): string =>
+    `https://pokeapi.co/api/v2/pokemon/${id}`;
+
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      const fetchedData: TPokemonData[] = [];
+
+      for (let i = 1; i < 152; i += 1) {
+        try {
+          const response = await fetch(pokemonUrl(i));
+          const { id, name, types }: TPokemonData = await response.json();
+          fetchedData.push({ id, name, types });
+        } catch (error) {
+          console.error('Failed to fetch Pokémon:', error);
+        }
+      }
+
+      setPokemonData(fetchedData);
+    };
+
+    fetchPokemon();
+  }, []);
+
+  return [pokemonData];
+};
+
+export default usePokemon;
