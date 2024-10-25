f43eb8301d029df3f8820ac27b8f1024dbfcfcbc
cloudjohnsp <jvavelino2017@gmail.com>
2024-10-25 20:07:40 -0300

    feat: creates data mock for dev

---
+++ src/Helpers/Pokemons/index.ts
@@ -0,0 +1,20 @@
+export const spriteUrl = (pokemonId: number): string =>
+  `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonId}.png`;
+
+export const gen1PokemonTypes = [
+  'normal',
+  'fire',
+  'water',
+  'electric',
+  'grass',
+  'ice',
+  'fighting',
+  'poison',
+  'ground',
+  'flying',
+  'psychic',
+  'bug',
+  'rock',
+  'ghost',
+  'dragon',
+];
