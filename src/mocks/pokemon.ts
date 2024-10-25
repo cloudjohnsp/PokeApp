da6dd8718d61d9314500187f4b56c5ef14320b82
cloudjohnsp <jvavelino2017@gmail.com>
2024-10-25 20:08:19 -0300

    feat: creates pokemon data mock for development

---
+++ src/mocks/pokemon.ts
@@ -0,0 +1,58 @@
+const pokemons = [
+  {
+    id: 1,
+    name: 'bulbasaur',
+    types: [{ type: { name: 'grass', url: 'string' } }],
+  },
+  {
+    id: 2,
+    name: 'ivysaur',
+    types: [
+      { type: { name: 'grass', url: 'string' } },
+      { type: { name: 'poison', url: 'string' } },
+    ],
+  },
+  {
+    id: 3,
+    name: 'venusaur',
+    types: [
+      { type: { name: 'grass', url: 'string' } },
+      { type: { name: 'poison', url: 'string' } },
+    ],
+  },
+  {
+    id: 4,
+    name: 'charmander',
+    types: [{ type: { name: 'fire', url: 'string' } }],
+  },
+  {
+    id: 5,
+    name: 'charmeleon',
+    types: [{ type: { name: 'fire', url: 'string' } }],
+  },
+  {
+    id: 6,
+    name: 'charizard',
+    types: [
+      { type: { name: 'fire', url: 'string' } },
+      { type: { name: 'flying', url: 'string' } },
+    ],
+  },
+  {
+    id: 7,
+    name: 'squirtle',
+    types: [{ type: { name: 'water', url: 'string' } }],
+  },
+  {
+    id: 8,
+    name: 'wartortle',
+    types: [{ type: { name: 'water', url: 'string' } }],
+  },
+  {
+    id: 9,
+    name: 'blastoise',
+    types: [{ type: { name: 'water', url: 'string' } }],
+  },
+];
+
+export default pokemons;
