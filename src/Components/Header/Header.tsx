da5280b288a98824f76b8eb284368905c53ba3dc
cloudjohnsp <jvavelino2017@gmail.com>
2024-10-31 11:48:32 -0300

    feat: adds user context data

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,12 +1,20 @@
 import './Header.css';
 import avatar from '../../assets/user.png';
 import applogo from '../../assets/pokeapp.png';
+import { Link } from 'react-router-dom';
+import { baseUrl } from '../../Helpers/Router';
+import { useContext } from 'react';
+import { AuthContext } from '../../Contexts/AuthContext';
 
 const Header = () => {
+  const { userData } = useContext(AuthContext);
+
   return (
     <header className='header-container'>
       <img className='header-img' src={applogo} alt='app logo' />
-      <img className='header-avatar' src={avatar} alt='user avatar' />
+      <Link to={`${baseUrl}/user/${userData.id}`}>
+        <img className='header-avatar' src={avatar} alt='user avatar' />
+      </Link>
     </header>
   );
 };
