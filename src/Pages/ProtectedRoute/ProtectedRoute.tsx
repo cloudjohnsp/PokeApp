e78a929917db489a6c1c4095aa279d41e45ba0c0
cloudjohnsp <jvavelino2017@gmail.com>
2024-10-31 11:49:39 -0300

    feat: adds token validation

--- src/Pages/ProtectedRoute/ProtectedRoute.tsx
+++ src/Pages/ProtectedRoute/ProtectedRoute.tsx
@@ -1,14 +1,33 @@
-import { ReactNode, useContext } from 'react';
-import { Navigate, useParams } from 'react-router-dom';
+import { ReactNode, useContext, useEffect } from 'react';
+import { Navigate, useNavigate, useParams } from 'react-router-dom';
 import { AuthContext } from '../../Contexts/AuthContext';
 import { baseUrl } from '../../Helpers/Router';
+import { jwtDecode } from 'jwt-decode';
+import { TDecodedToken } from '../../types/types';
 
 const ProtectedRoute = ({ children }: { children: ReactNode }) => {
   const { userId } = useParams();
   const { userData } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const sessionCookie = document.cookie;
+    if (!sessionCookie) {
+      navigate(`${baseUrl}`);
+    } else {
+      const currentTime = Date.now() / 1000;
+      const decodedToken = jwtDecode<TDecodedToken>(sessionCookie);
+
+      if (decodedToken) {
+        if (decodedToken.exp < currentTime) {
+          navigate(`${baseUrl}`);
+        }
+      }
+    }
+  });
 
   if (!userData || userData.id !== userId) {
-    return <Navigate to={`${baseUrl}/login`} replace />;
+    return <Navigate to={`${baseUrl}`} replace />;
   }
 
   return children;
