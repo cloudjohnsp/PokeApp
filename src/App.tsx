7e1e93e697934b49a860805f540a1a4ca8ec8b97
cloudjohnsp <jvavelino2017@gmail.com>
2024-10-31 11:50:54 -0300

    refactor: removes user validation from container

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,16 @@
-import { jwtDecode } from 'jwt-decode';
-import { baseUrl } from './Helpers/Router';
-import { useEffect, useState } from 'react';
-import { TDecodedToken } from './types/types';
-import { useNavigate } from 'react-router-dom';
 import Header from './Components/Header/Header';
 import Button from './Components/Button/Button';
 import FilterMenu from './Components/Filter/FilterMenu';
 import PokemonProvider from './Contexts/PokemonProvider';
 import PokemonList from './Components/PokemonList/PokemonList';
+import { useState } from 'react';
 
 function App() {
   const [showFilters, setShowFilters] = useState(false);
-  const navigate = useNavigate();
 
   const toggleFilters = () => {
     setShowFilters((prevShowFilters) => !prevShowFilters);
   };
-
-  useEffect(() => {
-    return () => {
-      const sessionCookie = document.cookie;
-      if (!sessionCookie) {
-        navigate(`${baseUrl}/login`);
-      } else {
-        const currentTime = Date.now() / 1000;
-        const decodedToken = jwtDecode<TDecodedToken>(sessionCookie);
-
-        if (decodedToken) {
-          if (decodedToken.exp < currentTime) {
-            navigate(`${baseUrl}/login`);
-          }
-        }
-      }
-    };
-  });
 
   return (
     <PokemonProvider>
