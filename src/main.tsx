1f0653e2b5032d7c2626d1bc5f7ec41d8c0bf4de
cloudjohnsp <jvavelino2017@gmail.com>
2024-10-31 11:49:10 -0300

    feat: registers profile route

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,14 @@ import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
 import App from './App';
 import { StrictMode } from 'react';
-import Auth from './Pages/Login/Auth';
+import Auth from './Pages/Auth/Auth';
 import { baseUrl } from './Helpers/Router';
 import { createRoot } from 'react-dom/client';
 import { ToastContainer } from 'react-toastify';
 import { AuthProvider } from './Contexts/AuthProvider';
 import ProtectedRoute from './Pages/ProtectedRoute/ProtectedRoute';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Profile from './Pages/Profile/Profile';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -16,6 +17,14 @@ createRoot(document.getElementById('root')!).render(
       <Router>
         <Routes>
           <Route path={`${baseUrl}`} element={<Auth />} />
+          <Route
+            path={`${baseUrl}/user/:userId`}
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
           <Route
             path={`${baseUrl}/main/:userId`}
             element={
